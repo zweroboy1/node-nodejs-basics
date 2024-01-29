d68853eb23b85f12c0986c0fc337cbcd8d8d4e4e
zweroboy1 <fprognoz@gmail.com>
2024-01-29 17:27:47 +0200

    feat: implement fs:create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+
+const FOLDER_NAME = 'files';
+const FILE_NAME = 'fresh.txt';
+const FILE_CONTENT = 'I am fresh and young';
+const FILE_EXISTS_ERROR_TEXT = 'FS operation failed';
+
 const create = async () => {
-    // Write your code here 
+  const __dirname = path.resolve(new URL(import.meta.url).pathname, '..');
+  const filePath = path.join(__dirname, FOLDER_NAME, FILE_NAME);
+  fs.writeFile(filePath, FILE_CONTENT, { flag: 'wx' }, (error) => {
+    if (error) {
+      if (error.code === 'EEXIST') {
+        throw new Error(FILE_EXISTS_ERROR_TEXT);
+      }
+      throw error;
+    }
+  });
 };
 
 await create();
