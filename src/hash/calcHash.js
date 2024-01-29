ee15ae4bdf37603dad66c989afc40483c067088c
zweroboy1 <fprognoz@gmail.com>
2024-01-29 22:07:54 +0200

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import { createReadStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream/promises';
+import { createHash } from 'crypto';
+
+const FOLDER_NAME = 'files';
+const FILE_NAME = 'fileToCalculateHashFor.txt';
+const HASH_ERROR_TEXT = 'Failed to calculate hash';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const targetFile = join(__dirname, FOLDER_NAME, FILE_NAME);
+  const readStream = createReadStream(targetFile);
+  const hash = createHash('sha256');
+
+  try {
+    await pipeline(readStream, hash);
+    console.log(hash.digest('hex'));
+  } catch {
+    throw new Error(HASH_ERROR_TEXT);
+  }
 };
 
 await calculateHash();
