3b05eea90312bd438b5f29bb1d03383623f0f8c4
zweroboy1 <fprognoz@gmail.com>
2024-01-29 20:01:02 +0200

    feat: implement fs:read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,27 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const FOLDER_NAME = 'files';
+const FILE_NAME = 'fileToRead.txt';
+const FILE_SYSTEM_ERROR_TEXT = 'FS operation failed';
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const sourceFile = path.join(__dirname, FOLDER_NAME, FILE_NAME);
+
+  try {
+    await fs.promises.access(sourceFile, fs.constants.R_OK);
+    const fileContent = await fs.promises.readFile(sourceFile, 'utf-8');
+    console.log(fileContent);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error(FILE_SYSTEM_ERROR_TEXT);
+    } else {
+      throw error;
+    }
+  }
 };
 
 await read();
