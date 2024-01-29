b892406b1bd78df826d2c1e26505cb840f396bc1
zweroboy1 <fprognoz@gmail.com>
2024-01-29 23:00:18 +0200

    feat: implement streams:transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
 const transform = async () => {
-    // Write your code here 
+  const reverse = new Transform({
+    transform(chunk, _encoding, callback) {
+      const reversedString = `${String(chunk).split('').reverse().join('')}\n`;
+      callback(null, reversedString);
+    },
+  });
+
+  await pipeline(process.stdin, reverse, process.stdout);
 };
 
 await transform();
