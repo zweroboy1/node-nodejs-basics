73f332ddaf8768e7d0c61afd3245f6ab6dcfb464
zweroboy1 <fprognoz@gmail.com>
2024-01-30 01:47:15 +0200

    feat: implement childPProccess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,23 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { spawn } from 'child_process';
+
+const FOLDER_NAME = 'files';
+const SCRIPT_NAME = 'script.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const childFilename = join(__dirname, FOLDER_NAME, SCRIPT_NAME);
+
+  const childProcess = spawn('node', [childFilename, ...args]);
+
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
+
+  childProcess.on('exit', (code) => {
+    console.log(`Exit with code: ${code}`);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2', 'someArgument3']);
