f88105153982975bd9a08a477f41d319cd685245
zweroboy1 <fprognoz@gmail.com>
2024-01-29 23:15:11 +0200

    fix: modify copy.js to work with nested folders

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path, { dirname } from 'path';
+import { cp } from 'fs';
+import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const FOLDER_NAME = 'files';
@@ -9,28 +9,23 @@ const FILE_SYSTEM_ERROR_TEXT = 'FS operation failed';
 const copy = async () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
-  const projectFolderPath = path.join(__dirname, FOLDER_NAME);
-  const newProjectFolderPath = path.join(__dirname, NEW_FOLDER_NAME);
+  const oldFolderPath = join(__dirname, FOLDER_NAME);
+  const newFolderPath = join(__dirname, NEW_FOLDER_NAME);
 
-  try {
-    await fs.promises.access(newProjectFolderPath, fs.constants.R_OK);
-    throw new Error();
-  } catch (error) {
-    if (error.code !== 'ENOENT') {
-      throw new Error(FILE_SYSTEM_ERROR_TEXT);
-    }
-    try {
-      const files = await fs.promises.readdir(projectFolderPath);
-      await fs.promises.mkdir(newProjectFolderPath);
-      for (const file of files) {
-        const projectFile = path.join(projectFolderPath, file);
-        const projectFileCopy = path.join(newProjectFolderPath, file);
-        await fs.promises.copyFile(projectFile, projectFileCopy);
+  cp(
+    oldFolderPath,
+    newFolderPath,
+    {
+      recursive: true,
+      force: false,
+      errorOnExist: true,
+    },
+    (err) => {
+      if (err) {
+        throw new Error(FILE_SYSTEM_ERROR_TEXT);
       }
-    } catch {
-      throw new Error(FILE_SYSTEM_ERROR_TEXT);
     }
-  }
+  );
 };
 
 await copy();
