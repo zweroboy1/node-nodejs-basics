05b8f9ef236d152f5595af50d774c3d9b2d6d9a1
zweroboy1 <fprognoz@gmail.com>
2024-01-29 21:42:57 +0200

    refactor: update CommonJS to ECMAScript

---
+++ src/modules/esm.mjs
@@ -0,0 +1,35 @@
+import path from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import { fileURLToPath } from 'url';
+import './files/c.js';
+
+const random = Math.random();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const fileToImport = random > 0.5 ? './files/a.json' : './files/b.json';
+const unknownObject = (await import(fileToImport, { assert: { type: 'json' } }))
+  .default;
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = createServerHttp((_, res) => {
+  res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log('To terminate it, use Ctrl+C combination');
+});
+
+export { unknownObject, myServer };
