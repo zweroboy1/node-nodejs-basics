5737d83d3b9c341ada8f075ddaa38b1fe0d53c90
zweroboy1 <fprognoz@gmail.com>
2024-01-29 18:22:56 +0200

    feat: implement fs:copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,36 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const PROJECT_FOLDER = 'files';
+const NEW_PROJECT_FOLDER = 'files_copy';
+const FILE_SYSTEM_ERROR_TEXT = 'FS operation failed';
+
 const copy = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const projectFolderPath = path.join(__dirname, PROJECT_FOLDER);
+  const newProjectFolderPath = path.join(__dirname, NEW_PROJECT_FOLDER);
+
+  try {
+    await fs.promises.access(newProjectFolderPath, fs.constants.R_OK);
+    throw new Error();
+  } catch (error) {
+    if (error.code !== 'ENOENT') {
+      throw new Error(FILE_SYSTEM_ERROR_TEXT);
+    }
+    try {
+      const files = await fs.promises.readdir(projectFolderPath);
+      await fs.promises.mkdir(newProjectFolderPath);
+      for (const file of files) {
+        const projectFile = path.join(projectFolderPath, file);
+        const projectFileCopy = path.join(newProjectFolderPath, file);
+        await fs.promises.copyFile(projectFile, projectFileCopy);
+      }
+    } catch {
+      throw new Error(FILE_SYSTEM_ERROR_TEXT);
+    }
+  }
 };
 
 await copy();
