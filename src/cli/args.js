1a544e287c725c2638ff4f6c11d0463a33c17907
zweroboy1 <fprognoz@gmail.com>
2024-01-29 21:15:49 +0200

    feat: implement cli:args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,23 @@
+const PREFIX = '--';
+const EQUAL_TEXT = ' is ';
+const SEPARATOR = ', ';
+
 const parseArgs = () => {
-    // Write your code here 
+  const entries = {};
+  const args = process.argv.slice(2);
+  for (let i = 0; i < args.length; i += 2) {
+    const key = args[i];
+    if (key.startsWith(PREFIX)) {
+      const value = args[i + 1];
+      entries[key.slice(2)] = value;
+    }
+  }
+
+  const argsString = Object.entries(entries)
+    .map(([key, value]) => `${key}${EQUAL_TEXT}${value}`)
+    .join(SEPARATOR);
+
+  console.log(argsString);
 };
 
 parseArgs();
